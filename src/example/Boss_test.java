dad8bba50048eddac768857ad23c45274dea1338
Illia <win.illia.777@gmail.com>
2014-03-04 01:54:54 +0200

    Comp.jpg

--- src/example/Boss_test.java
+++ src/example/Boss_test.java
@@ -22,143 +22,153 @@ import java.text.AttributedCharacterIterator;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import static Work.Workspace.toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 /**
  *
  * @author Илья
  */
 class Panet extends JPanel {
-    /*  public void draw_computer(Graphics g){
-     g.drawRect(40, 60, 50,40);//квадрат
-     g.drawLine(50,50,100,50);//  -
-     g.drawLine(50, 50,40,60);// /
-     g.drawLine(100, 50,90,60);// /
-     g.drawLine(100, 50,100,90);// |
-     g.drawLine(100, 90,90,100);// /
-        
-        
-        
-     }*/
 
-    public static int z, q;
+    /*
+     public static int z, q;
 
-    public void set_cord(int a, int b) {
-        z = a;
-        q = b;
+     public void set_cord(int a, int b) {
+     z = a;
+     q = b;
 
-    }
+     }
 
-    public void draw_rect(Graphics g) {
-      g.setColor(Color.blue);
-        int x1 = z - 15;
-        int x2 = x1 + 10;
-        int x3 = x2 + 30;
-        int x4 = x1 + 30;
+     public void draw_rect(Graphics g) {
+     g.setColor(Color.blue);
+     int x1 = z - 15;
+     int x2 = x1 + 10;
+     int x3 = x2 + 30;
+     int x4 = x1 + 30;
 
-        int y1 = q - 15;
-        int y3 = y1 - 10;
-        int y2 = y1 + 30;
-        int y4 = y3 + 30;
+     int y1 = q - 15;
+     int y3 = y1 - 10;
+     int y2 = y1 + 30;
+     int y4 = y3 + 30;
 
-        //draw computer    
-        g.drawRect(x1, y1, 30, 30);//квадрат
-        g.drawLine(x2, y3, x3, y3);//  -
-        g.drawLine(x2, y3, x1, y1);// /
-        g.drawLine(x3, y3, x4, y1);// /
-        g.drawLine(x3, y3, x3, y4);// |
-        g.drawLine(x3, y4, x4, y2);// /
-    }
+     //draw computer    
+     g.drawRect(x1, y1, 30, 30);//квадрат
+     g.drawLine(x2, y3, x3, y3);//  -
+     g.drawLine(x2, y3, x1, y1);// /
+     g.drawLine(x3, y3, x4, y1);// /
+     g.drawLine(x3, y3, x3, y4);// |
+     g.drawLine(x3, y4, x4, y2);// /
+     }
 
-    public void draw_switch(Graphics g) {
+     public void draw_switch(Graphics g) {
        
-g.setColor(Color.black);
-        int x = z - 15;
-        int y = q - 35;
-        g.drawRect(x, y, 70, 30);
+     g.setColor(Color.black);
+     int x = z - 15;
+     int y = q - 35;
+     g.drawRect(x, y, 70, 30);
 
+     }
+
+     public void draw_line(int a, int b, int a2, int b2, Graphics g) {
+     int x1 = a;
+     int y1 = b;
+     int x2 = a2;
+     int y2 = b2;
+     g.drawLine(x1, y1, x2, y2);
+
+     }
+
+     public void paintComponent(Graphics g) {
+
+     Graphics2D g2D = (Graphics2D)g;
+     BasicStroke pen1 = new BasicStroke(2);
+     g2D.setStroke(pen1);//Задает толщину линий
+        
+
+     super.paintComponent(g);
+
+     setBackground(Color.yellow);
+        
+     g.drawLine(340, 45, 210, 85);
+     g.drawLine(210,115,262,150);
+     g.drawLine(210,115,170,205);
+     g.drawLine(170, 235, 155, 280);
+     g.drawLine(170, 235, 260, 280); 
+     g.drawLine(340, 45, 340,80);
+     g.drawLine(340, 45, 460, 65);
+     g.drawLine(460, 95, 460, 145);
+     g.drawLine(460, 95, 390, 205);
+     g.drawLine(460, 95, 530, 205);
+     g.drawLine(390, 235,345,280);
+     g.drawLine(520, 235, 525, 280);
+     g.drawLine(520, 235, 450, 275);
+     g.drawLine(520, 235, 620, 275);
+     g.drawLine(620, 305, 605, 360);
+     g.drawLine(450, 305, 435, 360);
+        
+        
+     set_cord(320, 50);
+     draw_switch(g);//Switch №1
+     set_cord(200, 120);
+     draw_switch(g);//Hub №1
+     set_cord(440, 100);
+     draw_switch(g);//Switch №2
+     set_cord(335, 100);
+     draw_rect(g);//Computer №1
+     set_cord(455, 165);
+     draw_rect(g);//Computer №2
+     set_cord(370, 240);
+     draw_switch(g);//Hub №2 
+     set_cord(340, 300);
+     draw_rect(g);//Computer №3
+     set_cord(500, 240);
+     draw_switch(g);//Hub №3
+     set_cord(430, 310);
+     draw_switch(g);//Hub  №4
+     set_cord(520, 300);
+     draw_rect(g);//Computer №4
+     set_cord(600, 310);
+     draw_switch(g);//Hub №5
+     set_cord(430, 380);
+     draw_rect(g);//Computer №5
+     set_cord(600, 380);
+     draw_rect(g);//Computer №6
+     set_cord(260, 170);
+     draw_rect(g);//Compuer №7
+     set_cord(150, 240);
+     draw_switch(g);//Switch №3
+     set_cord(260, 300);
+     draw_rect(g);//Computer №9
+     set_cord(150, 300);
+     draw_rect(g);//Computer №8
+         
+     // g.drawString("dfdsfsdf", 200, 200);
+   
+     }*/
+    BufferedImage font,img;
+    Panet() {
+
+        try {
+            font =ImageIO.read(new File("C:\\Java_workbench\\Diplom\\General_image/Fon.jpg")); //BackGround image
+            img = ImageIO.read(new File("C:\\Java_workbench\\Diplom\\General_image/Comp_slow.png")); //Computer model
+        } catch (IOException ex) {
+            Logger.getLogger(Panet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+  
+    }
+    public void Panet() {
+   
     }
 
-    public void draw_line(int a, int b, int a2, int b2, Graphics g) {
-        int x1 = a;
-        int y1 = b;
-        int x2 = a2;
-        int y2 = b2;
-        g.drawLine(x1, y1, x2, y2);
-
-    }
-
-    public void paintComponent(Graphics g) {
-
- Graphics2D g2D = (Graphics2D)g;
-        BasicStroke pen1 = new BasicStroke(2);
-g2D.setStroke(pen1);//Задает толщину линий
-        
-
-        super.paintComponent(g);
-
-        setBackground(Color.yellow);
-        
-        g.drawLine(340, 45, 210, 85);
-        g.drawLine(210,115,262,150);
-        g.drawLine(210,115,170,205);
-        g.drawLine(170, 235, 155, 280);
-        g.drawLine(170, 235, 260, 280); 
-        g.drawLine(340, 45, 340,80);
-        g.drawLine(340, 45, 460, 65);
-        g.drawLine(460, 95, 460, 145);
-        g.drawLine(460, 95, 390, 205);
-        g.drawLine(460, 95, 530, 205);
-        g.drawLine(390, 235,345,280);
-        g.drawLine(520, 235, 525, 280);
-        g.drawLine(520, 235, 450, 275);
-        g.drawLine(520, 235, 620, 275);
-        g.drawLine(620, 305, 605, 360);
-        g.drawLine(450, 305, 435, 360);
-        
-        
-        set_cord(320, 50);
-        draw_switch(g);//Switch №1
-        set_cord(200, 120);
-        draw_switch(g);//Hub №1
-        set_cord(440, 100);
-        draw_switch(g);//Switch №2
-        set_cord(335, 100);
-        draw_rect(g);//Computer №1
-        set_cord(455, 165);
-        draw_rect(g);//Computer №2
-        set_cord(370, 240);
-        draw_switch(g);//Hub №2 
-        set_cord(340, 300);
-        draw_rect(g);//Computer №3
-        set_cord(500, 240);
-        draw_switch(g);//Hub №3
-        set_cord(430, 310);
-        draw_switch(g);//Hub  №4
-        set_cord(520, 300);
-        draw_rect(g);//Computer №4
-        set_cord(600, 310);
-        draw_switch(g);//Hub №5
-        set_cord(430, 380);
-        draw_rect(g);//Computer №5
-        set_cord(600, 380);
-        draw_rect(g);//Computer №6
-        set_cord(260, 170);
-        draw_rect(g);//Compuer №7
-        set_cord(150, 240);
-        draw_switch(g);//Switch №3
-        set_cord(260, 300);
-        draw_rect(g);//Computer №9
-        set_cord(150, 300);
-        draw_rect(g);//Computer №8
-        
-        
-    
-   
-        
-      
-        
-  // g.drawString("dfdsfsdf", 200, 200);
-   
+    public void paint(Graphics g) {
+   g.drawImage(font, 0, 0, getWidth(), getHeight(), null);
+   g.drawImage(img,100,100,null);
     }
 
 }
@@ -179,7 +189,7 @@ public class Boss_test extends JFrame {
 
         setTitle("Start page");
 
-        setSize(700, 500);
+        setSize(1000, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         toolkit = getToolkit();
         Dimension size = toolkit.getScreenSize();
@@ -190,6 +200,6 @@ public class Boss_test extends JFrame {
         add(new Panet());
          //pa.draw_universal(100,100);
 
-      //pa.draw_universal(100,300);
+        //pa.draw_universal(100,300);
     }
 }
--- src/example/Draw_line.java
+++ src/example/Draw_line.java
@@ -1,5 +1,6 @@
 package example;
 
+	
 
 
 import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.time;
@@ -16,71 +17,73 @@ import java.util.logging.Logger;
 import javax.swing.*;
 import javax.swing.WindowConstants;
 
+class Draw extends JPanel {
 
-class Draw extends JPanel /*implements Runnable*/  {
-
-     //   static Drawing frame;
-   
+public void Drawing() throws InterruptedException {
+//Thread.sleep(-1);
+setLayout(null);
 }
 
-public class Draw_line extends JFrame  {
-   
-    Draw panel;    public boolean status=true;
-      public static int x1 = 100, x2 = 120, y1 = 100, y2 = 100;
- public Draw_line() {
-      
- 
-       panel = new Draw();
-       
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500, 500);       
-      //  this.add(panel);
-      
-        this.setVisible(true);
-       while(true){
-   this.paint(this.getGraphics());}
- }
-    public void paint(Graphics g) {
-        
-     // super.paint(g);
-   //    Graphics2D g2D = (Graphics2D) g;
-    //   BasicStroke pen1 = new BasicStroke(5);
-    //  g2D.setStroke(pen1);
-        
-        g.setColor(Color.BLUE);      
-       try {
-           while (x2 <= 400) {   System.out.println("x1:" + x1 + " x2:" + x2);
-               g.drawLine(x1, y1, x2, y2);
-             
-                x1 += 30;
-                x2 += 30;
-           Thread.sleep(400);
-             //  this.repaint();
-               
-            }
-            x1 = 100;
-            x2 = 120;
-             
-            
-        } catch (InterruptedException ex) {
+public static   int x1 = 100, x2 = 120, y1 = 100, y2 = 100;
 
-            Logger.getLogger(Draw_line.class.getName()).log(Level.SEVERE, null, ex);
+public void paintComponents(Graphics g) {
+  
+//super.paint(g);
+    repaint();
+Graphics2D g2D = (Graphics2D) g;
+BasicStroke pen1 = new BasicStroke(5);
+g2D.setStroke(pen1);
 
-       }
-    }
-     
-          
+g.setColor(Color.BLUE); 
+try {
+while (x1 != 400) {
+     System.out.println("x1:" + x1 + " x2:" + x2);
+g.drawLine(x1, y1, x2, y2);
+x1 += 30;
+x2 += 30;
+Thread.sleep(150);
 
-       
-    
+}
+x1 = 100;
+x2 = 120;
+} catch (InterruptedException ex) {
 
-    public static void main(String[] args) throws InterruptedException {
-Draw_line draw = new Draw_line();
-      // while(draw.status ==true){
-         //  Thread.sleep(10);
-       
-               draw.setVisible(true);
-    
-      // }
+Logger.getLogger(Draw_line.class.getName()).log(Level.SEVERE, null, ex);
+
+}
+}
+
+}
+
+public class Draw_line extends JFrame {
+
+
+
+public Draw_line() {
+
+
+Draw panel = new Draw();
+
+this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+this.setSize(500, 500); 
+this.add(panel);
+
+this.setVisible(true);
+while(true){
+panel.paintComponents(panel.getGraphics());
+}
+
+
+
+
+}
+
+public static void main(String[] args) {
+
+
+
+new Draw_line().setVisible(true);
+
+
 }
 }
